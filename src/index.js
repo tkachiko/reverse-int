d944ffee82944b9e9c764e2d7cbc47317f2426e3
tkachiko <tkachikos@gmail.com>
2021-02-03 21:55:44 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        let reversed = n.toString().split("").slice(1).reverse().join("");
+        return reversed;
+    } else return n.toString().split("").reverse().join("");
+};
